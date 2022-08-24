4b307f4d19a3c2527bccdd4d0384902be7a3aca7
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-24 15:25:25 -0500

    need to complete and rewrite into arrow functions

--- script.js
+++ script.js
@@ -12,31 +12,17 @@ function filterOutOdds() {
 
 const filterOutOdds2 = ((...nums) => nums.filter(num => num%2 === 0));
 
-console.log(filterOutOdds2(1,2,3,4,5,6,7));
-
-
-
-
-
-
 
 // findMin
 // Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
 
 // Make sure to do this using the rest and spread operator.
 
-/*
+const findMin = (...numbers) => Math.min(...numbers);
+
 findMin(1,4,12,-3) // -3
 findMin(1,-1) // -1
 findMin(3,1) // 1
-
-
-
-
-
-
-
-
 
 
 // mergeObjects
@@ -44,12 +30,12 @@ findMin(3,1) // 1
 
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
+function mergeObjects (obj1, obj2) {
+  return {...obj1, ...obj2};
+}
 
-
-
-
-
-
+//online solution
+const mergeObjectsSolution = (obj1, obj2) => ({...obj1, ...obj2})
 
 // doubleAndReturnArgs
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
@@ -57,36 +43,32 @@ mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 
-
-
-
-
-
-
-
-
-
+function doubleAndReturnArgs (arr, ...others) {
+  return [...arr, ...others.map(n => n*2)];
+}
 
 // Slice and Dice!
 // For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
 
 // Make sure that you are always returning a new array or object and not modifying the existing inputs.
 
-/** remove a random element in the items array
+/* remove a random element in the items array
 and return a new array without that item. */
 
 function removeRandom(items) {
-
+  let rand = math.floor(Math.random()*items.length);
+  return [...items.slice(0,rand), ...items.slice(rand +1)];
 }
 
 
 
 
 
-/** Return a new array with every item in array1 and array2. */
+
+/* Return a new array with every item in array1 and array2. */
 
 function extend(array1, array2) {
-
+  return [...array1, ...array2];
 }
 
 
@@ -94,11 +76,13 @@ function extend(array1, array2) {
 
 
 
-/** Return a new object with all the keys and values
+/* Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 function addKeyVal(obj, key, val) {
-
+  let newObj = {...obj};
+  newObj[key] = val;
+  return newObj;
 }
 
 
@@ -107,10 +91,10 @@ function addKeyVal(obj, key, val) {
 
 
 
-/** Return a new object with a key removed. */
+/* Return a new object with a key removed. */
 
 function removeKey(obj, key) {
-
+  
 }
 
 
@@ -119,7 +103,7 @@ function removeKey(obj, key) {
 
 
 
-/** Combine two objects and return a new object. */
+/* Combine two objects and return a new object. */
 
 function combine(obj1, obj2) {
 
@@ -131,8 +115,14 @@ function combine(obj1, obj2) {
 
 
 
-/** Return a new object with a modified key and value. */
+/* Return a new object with a modified key and value. */
 
 function update(obj, key, val) {
 
 }
+
+
+
+
+
+//still need to refactor everything into arrow functions
